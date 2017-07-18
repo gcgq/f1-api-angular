7efecac726d40f57b01beb7a23b126a585a1162f
gcgq <gary.quiso@gmail.com>
2017-07-17 19:45:54 -0500

    added a filter to narrow results

--- js/app.js
+++ js/app.js
@@ -3,21 +3,20 @@ var f1App = angular.module("f1App",[]);
 f1App.controller("drvrCtrlr", ['$scope', '$http', function($scope, $http){
   $http.get("http://ergast.com/api/f1/2017/driverstandings.json").then(function(result){
     var driver_data = result.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-    var drvr_ary = [];
+    $scope.drivers = [];
     for (var i = 0; i < driver_data.length; i++) {
-      drvr_ary.push(driver_data[i].Driver);
+      $scope.drivers.push(driver_data[i].Driver);
     }
-    // console.log(drvr_ary);
-    // console.log(drvr_ary[0].givenName);
-    $scope.drivers = drvr_ary;
+    // console.log($scope.drivers);
     $scope.key = 'givenName';
-    // $scope.new_key = "";
-    $scope.sort_by_key = function(new_key){
+    $scope.sortByKey = function(new_key){
       if($scope.key === new_key){
+        //if the table is already sorted by the key, reverse the order
+        //by appending a '-' at the beginning of the key
         $scope.key = "-" + $scope.key;
       }
       else{
-        $scope.key = new_key
+        $scope.key = new_key;
       }
     }
   });
@@ -26,6 +25,5 @@ f1App.controller("drvrCtrlr", ['$scope', '$http', function($scope, $http){
 angular.module("f1App").directive("driverData", function(){
   return{
     templateUrl: "../templates/driver-data-template.html",
-    restrict: 'AE'
   }
 })
